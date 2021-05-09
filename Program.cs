4d9aaf2b06253b76a8c631fd7b0ce7af25dd6ad7
femaffi13 <fedemaffi13@gmail.com>
2021-05-09 12:31:45 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa de Cálculo de Área:");
+
+            Circulo miCirculo;      //Variable/objeto
+            miCirculo = new Circulo();      //Instanciar una clase, creación de ejemplar de Clase
+
+            Console.WriteLine(miCirculo.Area(5));
+
+            Circulo miCirculo2 = new Circulo();
+            Console.WriteLine(miCirculo2.Area(7));
+        }
+    }
+
+    class Circulo
+    {
+        const double PI = 3.1416;     //Las variables son propiedades de la Clase
+        //Campos de clase cuando están definidas dentro de una clase
+
+        public double Area(int radio){     //Método de Clase. Qué hace el objeto Circulo? (Comportamientos)
+            return PI * radio * radio;
+        }
+    }
+}
